7f446751003d8f3193d1f90d819e8eade4b0e10d
EmmyRecent <chukwuemekaeziagulu@gmail.com>
2026-03-07 23:48:12 +0000

    feat(nav): add mobile nav

---
+++ README.md
@@ -0,0 +1,17 @@
+## 🚀 Getting Started
+
+### 📥 Installation
+
+```bash
+# Clone the repo
+git clone https://github.com/EmmyRecent/event-like-a-movie.git
+
+# Navigate into the project
+cd event-like-a-movie
+
+# Install dependencies
+npm install
+
+# Run tailwindcss watch
+npm run watch
+```
---
+++ src/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerIcon = document.querySelector("#hamburger-icon");
+  const mobileNav = document.querySelector("#mobile-nav");
+  const mobileNavClose = document.querySelector("#mobile-nav-close");
+  const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
+
+  if (!hamburgerIcon || !mobileNav) return;
+
+  const openNav = () => {
+    mobileNav.classList.remove("hidden");
+  };
+
+  const closeNav = () => {
+    mobileNav.classList.add("hidden");
+  };
+
+  hamburgerIcon.addEventListener("click", () => {
+    const isHidden = mobileNav.classList.contains("hidden");
+    if (isHidden) {
+      openNav();
+    } else {
+      closeNav();
+    }
+  });
+
+  if (mobileNavClose) {
+    mobileNavClose.addEventListener("click", closeNav);
+  }
+
+  mobileNavLinks.forEach((link) => {
+    s;
+    link.addEventListener("click", closeNav);
+  });
+});
